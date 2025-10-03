4eaafd20c85703074f05d36590fa5788607016a0
hghrv <htchoumkeu@yahoo.fr>
2025-10-03 01:28:26 +0100

    Writing test titles for lambda handler

---
+++ test/test_lambda_handler.py
@@ -0,0 +1,37 @@
+class Test_Obfuscator_input:
+    def test_lambda_handler_downloads_csv_files_to_input_s3_bucket(self):
+        pass
+    def test_lambda_handler_downloads_json_files_to_intput_s3_bucket(self):
+        pass
+    def test_lambda_handler_downloads_parquet_files_to_input_s3_bucket(self):
+        pass
+
+class Test_Obfuscation:
+    def test_lambda_handler_obfuscates_csv_files_correctly(self):
+        pass
+    def test_lambda_handler_obfuscates_json_files_correctly(self):
+        pass
+    def test_lambda_handler_obfuscates_parquet_files_correctly(self):
+        pass
+
+class Test_Obfuscator_output:
+    def test_lambda_handler_uploads_csv_files_to_output_s3_bucket(self):
+        pass
+    def test_lambda_handler_uploads_json_files_to_output_s3_bucket(self):
+        pass
+    def test_lambda_handler_uploads_parquet_files_to_output_s3_bucket(self):
+        pass
+
+class Test_Obfuscator_returns_status_code_200_for_input_files_with_various_structures:
+    def test_lambda_handler_returns_status_code_200_with_files_containing_various_number_of_columns(self):
+        pass
+    def test_lambda_handler_returns_status_code_200_with_files_containing_various_number_of_rows(self):
+        pass
+    def test_lambda_handler_returns_status_code_200_with_files_containing_various_number_of_columns_to_obfucate(self):
+        pass
+    def test_lambda_handler_uploads_returns_status_code_200_with_files_containing_various_number_of_columns(self):
+        pass
+
+class Test_Obfuscator_catches_and_handles_errors_and_exceptions:
+     def Test_Obfuscator_catches_and_handles_errors_and_exceptions(self):
+         pass
