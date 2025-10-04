8c0f0cffcba4936ce2bf4075d0f0e5ac3c05c4e7
hghrv <htchoumkeu@yahoo.fr>
2025-10-04 23:06:26 +0100

    Creating test files for utility module functions

--- test/test_lambda_handler.py
+++ test/test_lambda_handler.py
@@ -1,4 +1,4 @@
-from layer2 import get_data, tranform_file_into_df
+from src import lambda_handler
 import logging
 import boto3
 from moto import mock_aws
@@ -7,7 +7,12 @@ import botocore.exceptions
 from unittest.mock import patch
 import pandas as pd
 
+# Json event parameter for lambda handler
+json_event = {
 
+}
+
+# Setting s3 bucket fixture for mock tests
 @pytest.fixture
 def mock_s3_bucket():
     with mock_aws():
@@ -18,22 +23,23 @@ def mock_s3_bucket():
             CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
 
-        test_files = ["test.csv", "test.json", "test.pkl"]
+        test_files = ["new_data/test_file.csv", "new_data/test_file.json", "new_data/test_file.pkl"]
         for file in test_files:
             s3.put_object(Bucket=bucket_name, Key=file)
 
-        with open("./test1.csv", "rb") as f:
+        with open("./new_data/test_file.csv", "rb") as f:
             body = f
-            s3.put_object(Bucket=bucket_name, Key="test1.csv", Body=body)
+            s3.put_object(Bucket=bucket_name, Key="test_file_1.csv", Body=body)
 
         yield bucket_name
 
+# defining tests
 class TestObfuscatorInput:
-    def test_lambda_handler_downloads_csv_files_to_input_s3_bucket(self, mock_s3_bucket):
+    def test_lambda_handler_downloads_csv_files_from_input_s3_bucket(self, mock_s3_bucket):
         pass
-    def test_lambda_handler_downloads_json_files_to_intput_s3_bucket(self, mock_s3_bucket):
+    def test_lambda_handler_downloads_json_files_from_input_s3_bucket(self, mock_s3_bucket):
         pass
-    def test_lambda_handler_downloads_parquet_files_to_input_s3_bucket(self, mock_s3_bucket):
+    def test_lambda_handler_downloads_parquet_files_from_input_s3_bucket(self, mock_s3_bucket):
         pass
 
 class TestObfuscatorOutput:
@@ -45,15 +51,15 @@ class TestObfuscatorOutput:
         pass
 
 class TestTranformationsIntoFileType:
-    def test_lambda_handler_tranfroms_file_into_df(self, mock_s3_bucket):
-        pass
-        # response = tranform_file_into_df("address.pkl", mock_s3_bucket)
+    # def test_lambda_handler_transforms_downloaded_file_into_df(self, mock_s3_bucket):
+        # pass
+        # response = load_into_dataframe("new_data/test_file.csv", mock_s3_bucket)
         # assert isinstance(response, pd.DataFrame)
 
     def test_lambda_handler_writes_output_file_into_bytestream(self, mock_s3_bucket):
         pass
-        # response = tranform_file_into_df("address.pkl", mock_s3_bucket)
-        # assert isinstance(response, pd.DataFrame)
+        # response = load_into_dataframe("new_data/test_file.csv", context)
+        # assert isinstance(response, csv.Bytestream)
 
 class TestObfuscationOfCsvFiles:
     def test_lambda_handler_obfuscates_csv_files_correctly(self, mock_s3_bucket):
@@ -63,7 +69,7 @@ class TestObfuscationOfCsvFiles:
     def test_lambda_handler_obfuscates_parquet_files_correctly(self, mock_s3_bucket):
         pass
 
-class TestObfuscatorReturnsStatusCode200ForInputFilesWithVariousSructures:
+class TestObfuscatorReturnsStatusCode200ForInputFilesWithVariousStructures:
     def test_lambda_handler_returns_status_code_200_with_files_containing_various_number_of_columns(self, mock_s3_bucket):
         pass
     def test_lambda_handler_returns_status_code_200_with_files_containing_various_number_of_rows(self, mock_s3_bucket):
@@ -73,6 +79,37 @@ class TestObfuscatorReturnsStatusCode200ForInputFilesWithVariousSructures:
     def test_lambda_handler_uploads_returns_status_code_200_with_files_containing_various_number_of_columns(self, mock_s3_bucket):
         pass
 
-class TestObfuscatorCatchesAndHandlesErrorsAndExceptions:
-     def Test_Obfuscator_catches_and_handles_errors_and_exceptions(self, mock_s3_bucket):
+class TestObfuscatorOutputHasExpectedObfuscatedContent:
+    def test_obfuscator_output_has_expected_obfuscated_content_for_csv_file(self, mock_s3_bucket):
          pass
+    def test_obfuscator_output_has_expected_obfuscated_content_for_json_file(self, mock_s3_bucket):
+         pass
+    def test_obfuscator_output_has_expected_obfuscated_content_for_pkl_file(self, mock_s3_bucket):
+         pass
+
+"""
+class TestObfuscatorCatchesAndHandlesErrorsAndExceptions:
+    
+    LOGGER = logging.getLogger(__name__)
+    
+    def Test_Obfuscator_catches_and_logs_errors(self, caplog):
+        expected_error_messsage = {
+            "Error": {
+                "Code": "NoSuchBucket",
+                "Message": "The specified bucket does not exist",
+            }
+        }
+        expected_error = botocore.exceptions.ClientError(
+            expected_error_messsage, "ListObjectsV2"
+        )
+
+        with patch("boto3.client") as mock_boto_client:
+            mock_s3 = mock_boto_client.return_value
+            mock_s3.list_objects_v2.side_effect = expected_error
+
+            with caplog.at_level(logging.ERROR):
+                #get_data("not-a-bucket")
+
+            assert "An error has occured with the client:" in caplog.text
+            assert #get_data("not-a-bucket") == {"Error": str(expected_error)}
+"""
---
+++ test/test_obfuscate.py
@@ -0,0 +1,34 @@
+from src import lambda_handler
+import logging
+import boto3
+from moto import mock_aws
+import pytest
+import botocore.exceptions
+from unittest.mock import patch
+import pandas as pd
+
+# Setting s3 bucket fixture for mock tests
+@pytest.fixture
+def mock_s3_bucket():
+    with mock_aws():
+        s3 = boto3.client("s3", region_name="eu-west-2")
+        bucket_name = "test-bucket"
+        s3.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+
+        test_files = ["new_data/test_file.csv", "new_data/test_file.json", "new_data/test_file.pkl"]
+        for file in test_files:
+            s3.put_object(Bucket=bucket_name, Key=file)
+
+        with open("./new_data/test_file.csv", "rb") as f:
+            body = f
+            s3.put_object(Bucket=bucket_name, Key="test_file_1.csv", Body=body)
+
+        yield bucket_name
+
+# defining tests
+class TestObfuscate:
+    def test_obfuscate(self, mock_s3_bucket):
+        pass
