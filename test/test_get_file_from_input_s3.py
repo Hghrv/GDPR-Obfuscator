112b3ab9f3a4e3268e5d35005fa778a7ecec4e92
hghrv <htchoumkeu@yahoo.fr>
2025-10-05 00:42:28 +0100

    Writing tests for test_get_file_from_input_s3.py

--- test/test_get_file_from_input_s3.py
+++ test/test_get_file_from_input_s3.py
@@ -1,11 +1,12 @@
-from src import lambda_handler
+from src.utils import get_file_from_input_s3
 import logging
+import csv
+import pandas as pd
 import boto3
 from moto import mock_aws
 import pytest
 import botocore.exceptions
 from unittest.mock import patch
-import pandas as pd
 
 # Setting s3 bucket fixture for mock tests
 @pytest.fixture
@@ -30,5 +31,40 @@ def mock_s3_bucket():
 
 # defining tests
 class TestGetFileFromInputS3:
-    def test_get_file_from_input_s3(self, mock_s3_bucket):
-        pass
+    def test_get_csv_file_from_input_s3(self, mock_s3_bucket):
+        
+        extracted = get_file_from_input_s3(mock_s3_bucket)
+        assert isinstance(extracted, dict)
+        assert "Data_extracted" in extracted            
+        assert len(extracted["Data_extracted"]) == 1
+        assert "new_data/test_file.csv" in extracted["Data_extracted"]
+            
+        response = csv.reader(extracted["Data_extracted"])
+        assert isinstance(response, csv.reader), "response is not a csv.reader instance"
+
+    LOGGER = logging.getLogger(__name__)
+    
+    def test_logs_when_file_is_extracted(self, caplog, mock_s3_bucket):
+        with caplog.at_level(logging.INFO):
+            get_file_from_input_s3(mock_s3_bucket, "new_data/test_file.csv")
+        assert "Extracting data from s3 bucket" in caplog.text
+
+    def test_logs_error(self, caplog):
+        expected_error_messsage = {
+            "Error": {
+                "Code": "NoSuchBucket",
+                "Message": "The specified bucket does not exist",
+            }
+        }
+        expected_error = botocore.exceptions.ClientError(
+            expected_error_messsage, "ListObjectsV2"
+        )
+
+        with patch("boto3.client") as mock_boto_client:
+            mock_s3 = mock_boto_client.return_value
+            mock_s3.list_objects_v2.side_effect = expected_error
+
+            with caplog.at_level(logging.ERROR):
+                get_file_from_input_s3("not-a-bucket")
+            assert "An error has occured with the client:" in caplog.text
+            assert get_file_from_input_s3("not-a-bucket") == {"Error": str(expected_error)}
